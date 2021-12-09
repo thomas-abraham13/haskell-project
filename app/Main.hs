53152561aaedd98329fc3fbdf91eeceb38f8ad6c
Thomas Abraham <thomasabraham832@gmail.com>
2021-12-09 18:30:33 +0000

    Added to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -9,16 +9,24 @@ import Lib
 
 main :: IO ()
 main = do
+    putStrLn "                                   "
     putStrLn "-----------------------------------"
     putStrLn "  NATIONAL BASKETBALL ASSOCIATION  "
-    putStrLn "   (1) - Download Player Data      "
-    putStrLn "   (4) - Exit Application          "
     putStrLn "-----------------------------------"
+    putStrLn "                                   "
+    putStrLn "   (1) - Download Player Data      "
+    putStrLn "   (0) - Exit Application          "
+    putStrLn "                                   "
+    putStrLn "-----------------------------------"
+    putStrLn "                                   "
     --conn <- initialiseDB
     hSetBuffering stdout NoBuffering
     putStr "Enter Choice : "
     option <- readLn :: IO Int
     case option of
-        4 -> print " Application Terminated "
+        0 -> print " Application Terminated "
              --print "       Thank You        "
-        otherwise -> print " Invalid Option "
+
+        otherwise -> do 
+                     print " Invalid Option " 
+                     main
