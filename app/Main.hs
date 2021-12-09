bd60c4e1ed7c96c5fcd631bfb3176d5aa26e8f03
Thomas Abraham <thomasabraham832@gmail.com>
2021-12-09 18:13:41 +0000

    Added to Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
+import System.IO
 import Lib
+--import Types
+--import Fetch
+--import Parse
+--import Database
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "-----------------------------------"
+    putStrLn "  NATIONAL BASKETBALL ASSOCIATION  "
+    putStrLn "   (1) - Download Player Data      "
+    putStrLn "   (4) - Exit Application          "
+    putStrLn "-----------------------------------"
+    --conn <- initialiseDB
+    hSetBuffering stdout NoBuffering
+    putStr "Enter Choice : "
+    option <- readLn :: IO Int
+    case option of
+        4 -> print " Application Terminated "
+             --print "       Thank You        "
+        otherwise -> print " Invalid Option "
