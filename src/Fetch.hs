4ae19b8f99e5a8442c7ddb76c29a28f3b3a52feb
Thomas Abraham <thomasabraham832@gmail.com>
2021-12-10 20:53:09 +0000

    Dependancies added to package.yaml

---
+++ src/Fetch.hs
@@ -0,0 +1,14 @@
+module Fetch (
+    download
+) where
+
+import qualified Data.ByteString.Lazy.Char8 as T
+import Network.HTTP.Simple
+
+type URL = String
+
+download :: URL -> IO T.ByteString
+download url = do
+    request <- parseRequest url
+    response <- httpLBS request
+    return $ getResponseBody response
