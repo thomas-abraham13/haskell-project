fcada46da0667e6192b7726879c83a706acbc99a
Thomas Abraham <thomasabraham832@gmail.com>
2021-12-10 03:27:35 +0000

    Formatting changes & Function added to Lib.hs

--- app/Main.hs
+++ app/Main.hs
@@ -16,6 +16,7 @@ main = do
     putStrLn "-----------------------------------"
     putStrLn "                                   "
     putStrLn "   (1) - Download Player Data      "
+    putStrLn "   (2) - Application Information   "
     putStrLn "   (0) - Exit Application          "
     putStrLn "                                   "
     putStrLn "-----------------------------------"
@@ -27,15 +28,28 @@ main = do
     putStrLn " "
     case option of
         0 -> do
-             print "                        "
-             print "------------------------"
-             print " Application Terminated "
-             print "       Thank You        "
-             print "------------------------"
-             print "                        "
+            print "                        "
+            print "------------------------"
+            print " Application Terminated "
+            print "       Thank You        "
+            print "------------------------"
+            print "                        "
+
+        1 -> do
+            print "-----------------------------------"
+            print "  NATIONAL BASKETBALL ASSOCIATION  "
+            print "-----------------------------------"
+            main
+
+        2 -> do
+            print "-----------------------------------"
+            print "  NATIONAL BASKETBALL ASSOCIATION  "
+            print "-----------------------------------"
+            conn <- someFunc
+            main
 
         otherwise -> do 
-                     print "                        "
-                     print "     Invalid Option     "
-                     print "                        "
-                     main
+                    print "                        "
+                    print "     Invalid Option     "
+                    print "                        "
+                    main
